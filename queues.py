f02c7c7241c1d3b1268cffbfe7999da1cadec20a
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-13 17:46:23 +0800

    Defined functions for enqueuing and dequeuing, and added comments.

--- queues.py
+++ queues.py
@@ -4,8 +4,10 @@ class Queue:
     def __init__(self):
         self._elements = deque()
 
+    # for adding element
     def enqueue(self, element):
         self._elements.append(element)
 
+    # for removing element
     def dequeue(self):
         return self._elements.popleft()
