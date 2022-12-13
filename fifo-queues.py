c8dd6229889c61aba871bd9d615076b693608097
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-13 23:04:59 +0800

    Added comments.

--- fifo-queues.py
+++ fifo-queues.py
@@ -1,10 +1,13 @@
 from queues import Queue
 
 fifo = Queue()
+
+# add element
 fifo.enqueue("1st")
 fifo.enqueue("2nd")
 fifo.enqueue("3rd")
 
+# remove and print element
 print(fifo.dequeue())
 print(fifo.dequeue())
 print(fifo.dequeue())
