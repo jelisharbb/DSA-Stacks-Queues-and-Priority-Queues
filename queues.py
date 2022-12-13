471fbeb8b9b7113347c0f10b126fa060fc44cea2
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-13 22:46:52 +0800

    Made the class iterable by implementing len and iter method.

--- queues.py
+++ queues.py
@@ -1,8 +1,18 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    # 
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    # for getting the length of deque
+    def __len__(self):
+        return len(self._elements)
+
+    # loop for dequeuing elements 
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
 
     # for adding element
     def enqueue(self, element):
