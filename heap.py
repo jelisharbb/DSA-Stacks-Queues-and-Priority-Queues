facad1e31bad01ad6ce971b2001c927672125fea
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 12:02:15 +0800

    Added elements using heappush, and printed them.

---
+++ heap.py
@@ -0,0 +1,11 @@
+from heapq import heappush
+
+fruits = []
+
+# adding elements
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+# resulting elements will not be sorted
+print(fruits)
