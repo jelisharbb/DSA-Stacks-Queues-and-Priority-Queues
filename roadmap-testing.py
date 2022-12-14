4057bd0ae6c7115a063a446c78f4dbaaac275f13
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 20:13:18 +0800

    Made a wrong commit. Just added comments.

--- roadmap-testing.py
+++ roadmap-testing.py
@@ -4,7 +4,7 @@ import networkx as nx
 # read and print the roadmap.dot file
 print(nx.nx_agraph.read_dot("roadmap.dot"))
 
-# --------------------------------------------
+# -------------------------------------------
 
 # access print the details in the node london
 graph = nx.nx_agraph.read_dot("roadmap.dot")
