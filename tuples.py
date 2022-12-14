1e23b64a9ab00e701503b07d94437ea498b98103
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 13:01:23 +0800

    Defined three tuples and compared their values.

---
+++ tuples.py
@@ -0,0 +1,8 @@
+# utilize the tuple to compare with heap
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+# checks their value
+print(person1 < person2)
+print(person2 < person3)
