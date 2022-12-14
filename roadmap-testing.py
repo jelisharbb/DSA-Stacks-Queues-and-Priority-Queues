b7a52d96bcdf0e16df11aca040c69d50d9b0fdeb
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 21:10:12 +0800

    Commented the previous testing; accessed the nodes and graph.

--- roadmap-testing.py
+++ roadmap-testing.py
@@ -1,11 +1,21 @@
-# module to be used
-import networkx as nx
+# # module to be used
+# import networkx as nx
 
-# read and print the roadmap.dot file
-print(nx.nx_agraph.read_dot("roadmap.dot"))
+# # read and print the roadmap.dot file
+# print(nx.nx_agraph.read_dot("roadmap.dot"))
 
 # -------------------------------------------
 
-# access print the details in the node london
-graph = nx.nx_agraph.read_dot("roadmap.dot")
-print(graph.nodes["london"])
+# # access print the details in the node london
+# graph = nx.nx_agraph.read_dot("roadmap.dot")
+# print(graph.nodes["london"])
+
+# -------------------------------------------
+
+# imported the class and class method from graph.py
+from graph import City, load_graph
+
+# call the load_graph class method, stored it in variables, the displayed it
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+print(nodes["london"])
+print(graph)
