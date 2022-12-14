2ef49e55de9a015a7f88e7b03f469ff55f801276
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 21:32:30 +0800

    Implemented a for loop that identifies the immediate neighbors of the specified city.

--- roadmap-testing.py
+++ roadmap-testing.py
@@ -6,13 +6,11 @@
 # # read and print the roadmap.dot file
 # print(nx.nx_agraph.read_dot("roadmap.dot"))
 
-# ----------------- test 2 --------------------------
-
 # # access print the details in the node london
 # graph = nx.nx_agraph.read_dot("roadmap.dot")
 # print(graph.nodes["london"])
 
-# ----------------- test 3 --------------------------
+# ----------------- test 2 --------------------------
 
 # imported the class and class method from graph.py
 from graph import City, load_graph
@@ -21,3 +19,8 @@ from graph import City, load_graph
 nodes, graph = load_graph("roadmap.dot", City.from_dict) # node variable - obtain a reference to an instance of the City class by the specified name; graph variable - holds the entire networkx Graph object
 print(nodes["london"])
 print(graph)
+
+print(f"\nNeighbors:")
+# identifies the immediate neighbors of the specified city using .neighbors() method
+for neighbor in graph.neighbors(nodes["london"]):
+    print(neighbor.name)
