62a73a971faf95ebd0a2bdda1e22dfab54403689
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 20:12:24 +0800

    Defined a class method that takes a dictionary of attributes extracted from a DOT file and returns a new instance of the City class.

--- graph.py
+++ graph.py
@@ -9,3 +9,13 @@ class City(NamedTuple):
     latitude: float
     longitude: float
 
+    # defines a class method that takes a dictionary of attributes extracted from a DOT file and returns a new instance of the City class
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name = attrs["xlabel"],
+            country = attrs["country"],
+            year = int(attrs["years"]) or None,
+            latitude = float(attrs["latitude"]),
+            longitude = float(attrs["longitude"]),
+        )
