dd10f9ed1b100547089f94ac97d4a33b0766f37b
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 22:56:53 +0800

    Defined functions and loop that identifies the neighboring cities and sort them by their distance.

--- roadmap-testing.py
+++ roadmap-testing.py
@@ -23,9 +23,22 @@ print(graph)
 print(f"\nNeighbors:")
 # loop that identifies the immediate neighbors of the specified city using .neighbors() method
 for neighbor in graph.neighbors(nodes["london"]):
-    print(neighbor.name)
+    print(neighbor.name) 
 
 print(f"\nNeighbors together with their respective distances:")
 # loop that identifies the immediate neighbors of the specified city and their respective distances
 for neighbor, weights in graph[nodes["london"]].items():
-    print(weights["distance"], neighbor.name)
+    print(weights["distance"], neighbor.name) # neighbors are always listed in the same order in which you defined them in the DOT file
+
+# function that returns the list of neighbors and their weights sorted by the specified strategy. the strategy takes a dictionary of all the weights associated with an edge and returns a sorting key
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key = lambda item: strategy(item[1]))
+
+# function that produces a floating-point distance based on the input dictionary
+def by_distance(weights):
+    return float(weights["distance"])
+
+print(f"\nNeighbors sorted by their distances:")
+# loop that iterate over the neighbors of London, sorted by distance in ascending order
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
