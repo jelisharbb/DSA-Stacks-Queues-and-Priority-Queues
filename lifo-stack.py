e091cfc15b5c3de1cd87ba9c9ae31f6275eab5f8
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 11:39:22 +0800

    Implemented another way of adding and removing elements.

--- lifo-stack.py
+++ lifo-stack.py
@@ -6,3 +6,18 @@ lifo = Stack("1st", "2nd", "3rd")
 # remove elements from the stack
 for element in lifo:
     print(element)
+
+
+# another way
+lifo = []
+
+# add elements to the stack
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+# remove elements from the stack
+print()
+print(lifo.pop())
+print(lifo.pop())
+print(lifo.pop())
