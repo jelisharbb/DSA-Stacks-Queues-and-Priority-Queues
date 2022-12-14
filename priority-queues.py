101091daaffd3328b56eceec01ff7cde14023267
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 13:51:49 +0800

    Defined a priority value. Enqueued elements with priority. Dequeued elements.

---
+++ priority-queues.py
@@ -0,0 +1,18 @@
+# get the PriorityQueue class from queues.py
+from queues import PriorityQueue
+
+# define a priority value
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+
+# enqueue elements
+messages.enqueueWithPriority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueueWithPriority(NEUTRAL, "Radio station tuned in")
+messages.enqueueWithPriority(CRITICAL, "Brake pedal depressed")
+messages.enqueueWithPriority(IMPORTANT, "Windshield wipers turned on")
+
+# supposedly dequeue elements based on their priority
+print(messages.dequeue())
