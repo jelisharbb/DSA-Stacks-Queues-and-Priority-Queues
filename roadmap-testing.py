a14cb6d7e2445cc2b1f7a78f071ca7269473b26f
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-14 20:11:06 +0800

    Defined a class method that takes a dictionary of attributes extracted from a DOT file and returns a new instance of the City class.

--- roadmap-testing.py
+++ roadmap-testing.py
@@ -4,6 +4,8 @@ import networkx as nx
 # read and print the roadmap.dot file
 print(nx.nx_agraph.read_dot("roadmap.dot"))
 
+# --------------------------------------------
+
 # access print the details in the node london
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 print(graph.nodes["london"])
