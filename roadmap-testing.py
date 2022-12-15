e1a4acc95c2f368bb4053282751bfe357ab06a65
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-15 22:19:29 +0800

    Implemented a loop that iterates over the neighbors of the specified city using DFS.

--- roadmap-testing.py
+++ roadmap-testing.py
@@ -152,3 +152,13 @@ from graph import City, load_graph
 # function that checks city with a year within 20th century
 def is_twentieth_century(year):
    return year and 1901 <= year <= 2000
+
+# loop that iterate over the neighbors of the specified city using depth-first search
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.dfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
