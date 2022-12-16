8f452f6487f6f299889fd0d54c3144c1c231371b
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-12-16 16:06:38 +0800

    Defined two functions: for entry point, and for arguments.

--- thread-safe-queues.py
+++ thread-safe-queues.py
@@ -1,3 +1,5 @@
+# this program implements the classic multi-producer, multi-consumer problem using Python’s thread-safe queues
+
 # import modules and classes
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
@@ -8,3 +10,17 @@ QUEUE_TYPES = {
     "lifo": LifoQueue,
     "heap": PriorityQueue
 }
+
+# this function is the entry point, which receives the parsed arguments supplied by parse_args()
+def main(args):
+    buffer = QUEUE_TYPES[args.queue]()
+
+# this function defines and add the default value for the queues
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
+    parser.add_argument("-p", "--producers", type=int, default=3)
+    parser.add_argument("-c", "--consumers", type=int, default=2)
+    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
+    return parser.parse_args()
